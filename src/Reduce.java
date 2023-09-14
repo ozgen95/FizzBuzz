6e83807d30bd988de65ba057035e05f46a334333
ozgen95 <ozgen.ates@mail.utoronto.ca>
2023-09-14 13:55:23 -0400

    Reduce completed

---
+++ src/Reduce.java
@@ -0,0 +1,16 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 100;
+        while (n != 0){
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {
+                n = n - 1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
